b9de6c1230c7dfe8cf144df897a75683339cdbe7
Raphaelle <raphaelle.clerici@gmail.com>
2020-04-14 14:50:44 +0200

    correction d'une erreur de nommage du script js

--- exo01/main.php
+++ exo01/main.php
@@ -5,8 +5,8 @@
     <title>Exo01</title>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://code.jquery.com/jquery-migrate-1.4.1.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
     <link rel="stylesheet" href="style.css">
-    <script type="text/javascript" src="monscript.js"></script>
 </head>
 
 <body>
